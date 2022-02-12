c6cffd1c9e58dc73e9b5616beff97a73ec3cabe9
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2022-02-12 15:56:29 +0800

    Completed - Adjusted and Changed Redundant Lines; Added Comments

--- CVCreator.py
+++ CVCreator.py
@@ -13,17 +13,20 @@ class PDF(FPDF):
     def marg_(self):
         self.set_margins(15, 15, 15)
 
-    def pg_header (self):
+    def pg_header (self): 
+        # Header == Display the User's ID picture and Name
         self.image("ID_2x2.jpg", 160, 11, 37, 35)
-        self.set_font("helvetica", "B", 23)
+        self.set_font("helvetica", "B", 31)
         for i in range(1,2):
             self.cell(0, 10, "", border=False, ln=True)
         self.cell(40)
         self.set_xy(35,20)
         self.cell(100, 12, usrDetails[0]['name'], border=False, ln=True, align="C")
-        self.ln(7)
+        self.ln(5)
 
+# Start of the 'Content' Segment
     def rsme_crrobj(self):
+        # Formatted subheader for Career Objectives
         self.set_fill_color(45,45,45)
         self.set_text_color(255,255,255)
         self.set_font("helvetica", "", 8)
@@ -31,13 +34,15 @@ class PDF(FPDF):
         self.ln(4)
     
     def crrobj_cntnt(self):
+        # Uses JSON Content for Displaying the Objective
         self.set_font("helvetica", "", 9)
         self.set_text_color(0,0,0)
         self.set_x(20)
-        self.multi_cell(133,4,"To find a position where I am able to transmit my knowledge gained through my chosen course. In addition to grow and seek experience of the field I am pursuing.")
+        self.multi_cell(133,4, usrDetails[1]['objective'])
         self.ln()
 
     def rsme_personal(self):
+        # Formatted subhead for Personal Information
         self.set_fill_color(45,45,45)
         self.set_text_color(255,255,255)
         self.set_font("helvetica", "", 8)
@@ -45,12 +50,14 @@ class PDF(FPDF):
         self.ln(2)
 
     def personal_cntnt(self):
+        # Text Configurations
         self.set_font("helvetica", "", 9)
         self.set_text_color(0,0,0)
+        # Lists all needed dict_keys
         infoCateg = list(usrDetails[1]['personalInfo'].keys())
         maxPersonal = len(infoCateg)
 
-        for i in range(maxPersonal):
+        for i in range(maxPersonal): # Iteration of Similar Outputs - Uses the number of keys as range
             if i % 2 == 0:
                 self.set_x(20)
                 self.cell(20, 4, infoCateg[i], border=False, ln=False)
@@ -63,6 +70,7 @@ class PDF(FPDF):
         self.ln(3)
 
     def rsme_contact(self):
+        # Formatted subhead for Contact Info
         self.set_fill_color(45,45,45)
         self.set_text_color(255,255,255)
         self.set_font("helvetica", "", 8)
@@ -70,6 +78,7 @@ class PDF(FPDF):
         self.ln(2)
     
     def contact_cntnt(self):
+        # Manual Setup -- Uses JSON Contents for Display by accessing dict with dict_keys
         self.set_font("helvetica", "", 9)
         self.set_text_color(0,0,0)
         self.set_x(20)
@@ -89,10 +98,10 @@ class PDF(FPDF):
         self.cell(25, 4, "Email Address", border=False, ln=False)
         self.cell(6, 4, content_sep, border=False, ln=False, align="C")
         self.cell(40, 4, usrDetails[1]['contact']['Email Address'], border=False, ln=True) 
-
         self.ln(3)
 
     def rsme_educ(self):
+        # Formatted subhead for Educational Background
         self.set_fill_color(45,45,45)
         self.set_text_color(255,255,255)
         self.set_font("helvetica", "", 8)
@@ -100,57 +109,72 @@ class PDF(FPDF):
         self.ln(5)
 
     def educ_cntnt(self):
-        self.set_font("helvetica", "", 8)
+        # Uses JSON Contents for Display -- iteration (For Loops)
+        self.set_font("helvetica", "", 9)
         self.set_text_color(0,0,0)
         self.set_x(35)
         self.cell(25, 4, "Tertiary", border=False, ln=False)
         self.cell(16, 4, "", border=False, ln=False, align="C")
-        self.cell(40, 4, usrDetails[1]['education']['Tertiary']['Major'], border=False, ln=True)
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['Tertiary']['University'], border=False, ln=True) 
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['Tertiary']['Location'], border=False, ln=True) 
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['Tertiary']['Year'], border=False, ln=True) 
+        educCateg = list(usrDetails[1]['education']['Tertiary'].values())
+        maxEduc = len(educCateg)
+
+        for i in range(maxEduc):
+            if i == 0:
+                self.cell(40, 4, educCateg[i], border=False, ln=True)
+            else:
+                self.set_x(76) 
+                self.cell(40, 4, educCateg[i], border=False, ln=True) 
         self.ln(2)
 
         self.set_x(35)
         self.cell(25, 4, "Secondary", border=False, ln=False)
         self.cell(16, 4, "", border=False, ln=False, align="C")
-        self.cell(40, 4, usrDetails[1]['education']['High School']['Senior Level']['Strand'], border=False, ln=True)
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['High School']['Senior Level']['School'], border=False, ln=True) 
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['High School']['Senior Level']['Location'], border=False, ln=True) 
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['High School']['Senior Level']['Year'], border=False, ln=True)
+        educCateg_hs_sen = list(usrDetails[1]['education']['High School']['Senior Level'].values())
+        maxEduc_s = len(educCateg_hs_sen)
+
+        for i in range(maxEduc_s):
+            if i == 0:
+                self.cell(40, 4, educCateg_hs_sen[i], border=False, ln=True)
+            else:
+                self.set_x(76) 
+                self.cell(40, 4, educCateg_hs_sen[i], border=False, ln=True) 
         self.ln(3)
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['High School']['Junior Level']['School'], border=False, ln=True) 
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['High School']['Junior Level']['Location'], border=False, ln=True) 
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['High School']['Junior Level']['Year'], border=False, ln=True)
+
+        educCateg_hs_jr = list(usrDetails[1]['education']['High School']['Junior Level'].values())
+        maxEduc_j = len(educCateg_hs_jr)
+
+        for i in range(maxEduc_j):
+            self.set_x(76) 
+            self.cell(40, 4, educCateg_hs_jr[i], border=False, ln=True) 
         self.ln(2)
 
         self.set_x(35)
         self.cell(25, 4, "Primary", border=False, ln=False)
         self.cell(16, 4, "", border=False, ln=False, align="C")
-        self.cell(40, 4, usrDetails[1]['education']['Elementary']['School_0'], border=False, ln=True)
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['Elementary']['Location_0'], border=False, ln=True) 
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['Elementary']['Year_0'], border=False, ln=True)
-        self.ln(3) 
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['Elementary']['School_1'], border=False, ln=True) 
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['Elementary']['Location_1'], border=False, ln=True) 
-        self.set_x(76) 
-        self.cell(40, 4, usrDetails[1]['education']['Elementary']['Year_1'], border=False, ln=True) 
+        educCateg_prm = list(usrDetails[1]['education']['Elementary'].values())
+        maxEduc_prm = len(educCateg_prm)
+
+        repeatnum = 0
+        for i in range(maxEduc_prm): # Continuous format regardless of numbers of info batches entered
+            if i == 0:
+                self.cell(40, 4, educCateg_prm[i], border=False, ln=True)
+            elif i <= 2:
+                self.set_x(76) 
+                self.cell(40, 4, educCateg_prm[i], border=False, ln=True)
+                if repeatnum == 2:
+                    self.ln(3)
+                    repeatnum = 0
+            elif i > 2:
+                self.set_x(76) 
+                self.cell(40, 4, educCateg_prm[i], border=False, ln=True)
+                if repeatnum == 3:
+                    self.ln(3)
+                    repeatnum = 0
+            repeatnum  += 1
         self.ln(3)
 
     def rsme_skillst(self):
+        # Formatted subhead for Skill Sets
         self.set_fill_color(45,45,45)
         self.set_text_color(255,255,255)
         self.set_font("helvetica", "", 8)
@@ -158,9 +182,10 @@ class PDF(FPDF):
         self.ln(4)
 
     def skill_cntnt(self):
-        self.set_font("helvetica", "", 8)
+        # Double column format, determines the greater value set and add empty fillers to the lower value set.
+        self.set_font("helvetica", "", 9)
         self.set_text_color(0,0,0)
-        self.set_x(20)
+        self.set_x(16)
         self.cell(90, 5, "Technical Skills", ln=False)
         self.cell(20, 5, "Soft Skills", ln=True, align="R")
         prsnlSkills = len(usrDetails[1]['skills']['Personal'])
@@ -180,9 +205,9 @@ class PDF(FPDF):
                     Valuepr_skill.append(empty_filler)
 
                 for i in range(maxRange):
-                    self.set_x(35)
-                    self.cell(90, 5, usrDetails[1]["skills"]["Technical"][i], ln=False)
-                    self.cell(20, 5, Valuepr_skill[i], ln=True)
+                    self.set_x(22)
+                    self.cell(97, 5, usrDetails[1]["skills"]["Technical"][i], ln=False)
+                    self.cell(30, 5, Valuepr_skill[i], ln=True)
 
             else:
                 maxRange = prsnlSkills
@@ -192,12 +217,13 @@ class PDF(FPDF):
                     Valuepr_skill.append(empty_filler)
 
                 for i in range(maxRange):
-                    self.set_x(35)
-                    self.cell(90, 5, Valuepr_skill[i], ln=False)
-                    self.cell(20, 5, usrDetails[1]["skills"]["Personal"][i], ln=True)
+                    self.set_x(22)
+                    self.cell(97, 5, Valuepr_skill[i], ln=False)
+                    self.cell(30, 5, usrDetails[1]["skills"]["Personal"][i], ln=True)
         self.ln(4)
 
     def rsme_achvmnt(self):
+        # Formatted subhead for Achievements
         self.set_fill_color(45,45,45)
         self.set_text_color(255,255,255)
         self.set_font("helvetica", "", 8)
@@ -205,7 +231,8 @@ class PDF(FPDF):
         self.ln(3)
 
     def achvment_cntnt(self):
-        self.set_font("helvetica", "", 8)
+        # Simple For-Loop Iteration for easier display
+        self.set_font("helvetica", "", 9)
         self.set_text_color(0,0,0)
         maxAchvment = len(usrDetails[1]['achvment'])
         for i in range(maxAchvment):
@@ -214,34 +241,40 @@ class PDF(FPDF):
         self.ln(5)
 
     def rsme_ref(self):
+        # Formatted subhead for References
         self.set_fill_color(45,45,45)
         self.set_text_color(255,255,255)
         self.set_font("helvetica", "", 8)
         self.cell(0, 5, content_hdr[5], border=False, ln=True, align="C", fill=True)
-        self.ln(5)
+        self.ln(3)
     
     def ref_cntnt(self):
-        self.set_font("helvetica", "", 8)
+        # Simple cell display
+        self.set_font("helvetica", "I", 9)
         self.set_text_color(0,0,0)
         self.set_x(20)
         self.cell(20, 5, usrDetails[1]['reference'])
         self.ln(5)
 
-content_hdr = ["PERSONAL PARTICULARS", "CONTACT INFORMATION", "EDUCATIONAL BACKGROUND", "SKILL SETS", "ACHIEVEMENTS", "REFERENCES", "CAREER OBJECTIVES"]
-
-content_sep = ":"
-
+# File Detection and Handling
 fileName = "RELLEVE_json.json"
 
 try:
     verifyData = os.path.exists(fileName)
+    if verifyData == True:
+        print("\33[92mWait a moment.. Creating your file :)\33[0m")
 except Exception as e:
-    print(e)
+    print(e, "\33[91mPlease include the JSON file to your current directory :(\33[0m")
 
 with open(fileName, "r") as jsFile:
     usrDetails = json.load(jsFile)
 
-pdf = PDF("P", "mm", "A4")
+content_hdr = ["PERSONAL PARTICULARS", "CONTACT INFORMATION", "EDUCATIONAL BACKGROUND", "SKILL SETS", "ACHIEVEMENTS", "REFERENCES", "CAREER OBJECTIVES"]
+
+content_sep = ":"
+
+# PDF Properties -- to be filled by data from JSON file
+pdf = PDF("P", "mm", (210,305))
 pdf.set_auto_page_break(auto=True, margin=15)
 pdf.add_page()
 pdf.marg_()
@@ -261,4 +294,5 @@ pdf.achvment_cntnt()
 pdf.rsme_ref()
 pdf.ref_cntnt()
 
+# Call for Final Output 
 pdf.output("RELLEVE_JO-LYNDON.pdf")
